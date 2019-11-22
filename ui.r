9653cd1ef2cc12e50ddc3b7235b857c5dd08abc4
Nickolay Brazhnik <35973749+brazhira@users.noreply.github.com>
2019-11-22 16:10:22 +0300

    Add files via upload

---
+++ ui.r
@@ -0,0 +1,45 @@
+library(shiny)
+library(leaflet)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Мониторинга снега", titleWidth = 300),
+  dashboardSidebar(
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "shinychat.css")
+    ),
+    width = 300,
+    tabsetPanel(
+      tabPanel("Comments",
+               fluidPage(
+                 fluidRow( 
+                   uiOutput("chat"),
+                   textInput("entry", ""),
+                   actionButton("send", "Send"),
+                   actionButton("submit","Mark")
+                 )
+               )
+      ),
+      tabPanel("Photos",
+               fluidPage(
+                 fluidRow( 
+                   uiOutput("photofeed"),
+                   fileInput("file","File") 
+                 )
+               )
+      )
+    )
+  ),
+  dashboardBody(
+    tabsetPanel(
+      tabPanel("Карта",
+               leafletOutput("murmanskmap",height = "90vh")),
+      tabPanel("Статистика",
+               actionButton("stats", label = "Убрать снег"),
+               actionButton("filldata", label = "Заполнить данными"),
+               actionButton("route", label="Нарисовать путь"),
+               dataTableOutput("table")
+               ))
+    
+  )
+)
