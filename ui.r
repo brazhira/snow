ca1341af274918a7cd8798b0c22a55b87094aa7c
brazhira <35973749+brazhira@users.noreply.github.com>
2019-11-25 03:30:28 +0300

    second_update

--- ui.r
+++ ui.r
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 library(shiny)
 library(leaflet)
 library(shinydashboard)
 
 ui <- dashboardPage(
-  dashboardHeader(title = "Мониторинга снега", titleWidth = 300),
+  dashboardHeader(title = "Мониторинг снега", titleWidth = 300),
   dashboardSidebar(
     tags$head(
       tags$link(rel = "stylesheet", type = "text/css", href = "shinychat.css")
@@ -44,50 +43,3 @@ ui <- dashboardPage(
     
   )
 )
-=======
-library(shiny)
-library(leaflet)
-library(shinydashboard)
-
-ui <- dashboardPage(
-  dashboardHeader(title = "Мониторинга снега", titleWidth = 300),
-  dashboardSidebar(
-    tags$head(
-      tags$link(rel = "stylesheet", type = "text/css", href = "shinychat.css")
-    ),
-    width = 300,
-    tabsetPanel(
-      tabPanel("Comments",
-               fluidPage(
-                 fluidRow( 
-                   uiOutput("chat"),
-                   textInput("entry", ""),
-                   actionButton("send", "Send"),
-                   actionButton("submit","Mark")
-                 )
-               )
-      ),
-      tabPanel("Photos",
-               fluidPage(
-                 fluidRow( 
-                   uiOutput("photofeed"),
-                   fileInput("file","File") 
-                 )
-               )
-      )
-    )
-  ),
-  dashboardBody(
-    tabsetPanel(
-      tabPanel("Карта",
-               leafletOutput("murmanskmap",height = "90vh")),
-      tabPanel("Статистика",
-               actionButton("stats", label = "Убрать снег"),
-               actionButton("filldata", label = "Заполнить данными"),
-               actionButton("route", label="Нарисовать путь"),
-               dataTableOutput("table")
-               ))
-    
-  )
-)
->>>>>>> d50e671a5ac45d56f0a3f2d466f228a88ad763dd
